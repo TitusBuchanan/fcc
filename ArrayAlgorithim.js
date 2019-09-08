649770a800ae23283e3bc8d9aa48a9228d110577
Titus Buchanan Jr <titusbuchananjr@gmail.com>
2019-09-08 15:13:04 -0400

    changed a number

--- ArrayAlgorithim.js
+++ ArrayAlgorithim.js
@@ -1,5 +1,5 @@
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+largestOfFour([[4, 5, 1, 234], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 
 function largestOfFour(array) {
     var results = [];
@@ -16,3 +16,4 @@ function largestOfFour(array) {
 
     return results;
 }
+
