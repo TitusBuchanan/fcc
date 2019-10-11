ee15f1ff27f4c33e3cca2bafd5834629ac9a6807
titus buchanan <titusbuchananjr@gmail.com>
2019-10-11 15:45:17 -0400

    new program that shows off the months of the year with the season

---
+++ weather.js
@@ -0,0 +1,13 @@
+var seasons = ["Summer","Winter"];
+var months = [ ["June","July", "August", "May"] , ["February","December","January","Realistically the end of november"] ];
+
+console.log("These are some of the best months of the year");
+
+
+for (var i = 0; i <months.length; i++){
+    for(var j = 0; j < months[i].length; j++){
+console.log(months[i][j]);
+    }
+    //console.log(months[i][j]);
+
+    }
