c3d2dc62cc6db032a9e83809de5a045bfbf6e8fe
titus buchanan <titusbuchananjr@gmail.com>
2019-10-14 23:46:30 -0400

    program practicng the principle of objects in Javacript

---
+++ objectprac.js
@@ -0,0 +1,12 @@
+var titusLife = {
+    age:27,
+    height:75,
+    weight:215,
+    homeTown:["Roselle, New Jersey"],
+    sport:["Football"],
+    Nationlity:["Dominican"],
+    College:["Saint Anselm College"]
+
+}
+
+console.log("Titus is",titusLife.age,"Years Old and weighs", titusLife.weight, "Pounds");
