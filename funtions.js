b2a1c9d4180dc555f772c1d81681c81bff94dbba
titus buchanan <titusbuchananjr@gmail.com>
2019-10-16 13:57:57 -0400

    practice with javascript functions

---
+++ funtions.js
@@ -0,0 +1,20 @@
+
+var sentence = "This is just a tester sentence to test functions"
+
+function splitSentence(sentenceString){
+    var afterSplit = sentenceString.split(",");
+    return afterSplit;
+}
+
+console.log(splitSentence(sentence));
+
+
+weekendProfit =  2350
+numberOfWeekends = 52
+
+function totalProfit(profit , weekends){
+var total = (profit * weekends);
+return total;
+}
+
+console.log(totalProfit(weekendProfit,numberOfWeekends));
